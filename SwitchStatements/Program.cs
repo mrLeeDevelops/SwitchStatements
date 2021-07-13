0fd40bd498eb20265dcdc758eeb9c476942bf198
mrLeeDevelops <mrlee.truecoder@gmail.com>
2021-07-13 17:06:55 -0500

    Initial Commit

---
+++ SwitchStatements/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SwitchStatements
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("What is yiur favorite subject in school?");
+        }
+    }
+}
