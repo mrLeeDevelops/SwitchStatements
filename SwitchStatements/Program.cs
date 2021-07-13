42cd0f85c39c62d237a7077cb73c56fa2c70fdef
mrLeeDevelops <mrlee.truecoder@gmail.com>
2021-07-13 17:52:31 -0500

    Added switch logic.

--- SwitchStatements/Program.cs
+++ SwitchStatements/Program.cs
@@ -6,7 +6,27 @@ namespace SwitchStatements
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("What is yiur favorite subject in school?");
+            Console.WriteLine("What is your favorite subject in school?");
+            var response = Console.ReadLine();
+
+            switch (response)
+            {
+                case "Math":
+                    Console.WriteLine($"{response} is cool!");
+                    break;
+
+                case "Science":
+                    Console.WriteLine($"{response} is cool. There are so many types of {response}.");
+                    break;
+
+                case "English":
+                    Console.WriteLine($"If only I could read {response}, then I would know what your response is. :-)");
+                    break;
+
+                default:
+                    Console.WriteLine($"{response} huh? Never heard of that one but I'm sure it's cool!");
+                    break;
+            }
         }
     }
 }
